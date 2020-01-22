f227fa5159f5b5f5958230f208bf1aa4343017c2
Michelle Johnson <bob1234girl@gmail.com>
2020-01-22 10:05:28 -0600

    completed exercise

---
+++ cohort.py
@@ -0,0 +1,8 @@
+class Cohort:
+
+    def __init__(self, name):
+        # Establish the properties of each book
+        # with a default value
+        self.name = name
+        self.students = []
+        self.instructors = []
---
+++ exercise.py
@@ -0,0 +1,7 @@
+class Exercise:
+
+    def __init__(self, name, language):
+        # Establish the properties of each book
+        # with a default value
+        self.name = name
+        self.language = language
